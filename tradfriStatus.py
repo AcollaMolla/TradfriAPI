92dffb56375e08664b92735f858b0ee3771b6c52
Anton <37123861+AcollaMolla@users.noreply.github.com>
2019-10-24 15:29:06 +0200

    Added credit

--- tradfriStatus.py
+++ tradfriStatus.py
@@ -1,3 +1,20 @@
+# file        : tradfri/tradfriStatus.py
+# purpose     : getting status from the Ikea tradfri smart lights
+#
+# author      : harald van der laan
+# date        : 2017/11/01
+# version     : v1.2.0
+#
+# changelog   :
+# - v1.2.0      update for new gateway 1.1.15 issues                    (harald)
+# - v1.1.0      refactor for cleaner code                               (harald)
+# - v1.0.0      initial concept                                         (harald)
+
+"""
+    tradfriStatus.py - module for getting status of the Ikea tradfri smart lights
+    This module requires libcoap with dTLS compiled, at this moment there is no python coap module
+    that supports coap with dTLS. see ../bin/README how to compile libcoap with dTLS support
+"""
 import sys
 import os
 import json
